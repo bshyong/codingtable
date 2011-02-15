6aafa5c2664294954e6b1158a38ac477d091d05e
Ben Shyong <likeachild@gmail.com>
2011-02-15 12:47:18 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodingTable::Application.config.secret_token = 'e63106fe8c031d464dbbd821a21936fca8141d25b781795cb27bb913d6f7eed8968034e9aa00fad9b6810ae4c6e945228cd70946057f63cad9ac97f60976365d'
---
+++ db/migrate/20110215173706_create_languages.rb
@@ -0,0 +1,16 @@
+class CreateLanguages < ActiveRecord::Migration
+  def self.up
+    create_table :languages do |t|
+      t.string :name
+      t.integer :year
+      t.string :generation
+      t.string :category
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :languages
+  end
+end
