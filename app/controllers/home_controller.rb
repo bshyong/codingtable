04101344de96e783f62295a35715e01849f883ef
Ben Shyong <likeachild@gmail.com>
2011-02-22 02:26:50 -0500

    added columns

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,20 @@
 class HomeController < ApplicationController
   def index
     @languages = Language.all
+    @heights = [9, 9, 7, 7, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 8, 8]
+    @columns = Array.new
+
+    @heights.each_with_index do |h, index|
+      @columns << @languages.slice!(0, h)
+    end
+
+   if @columns.last.empty?
+        @columns.pop
+    end
+
+#    @heights.each_with_index do |h, index|
+#    eval("#{@column+index.to_s} = #{@languages.slice!(0, h)}")
+#    end
   end
 
   def admin
