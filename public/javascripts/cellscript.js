57234b794f2b46ed01b5e8c753f5a11b87e2298f
unknown <Alice@.(none)>
2011-02-22 04:22:11 -0500

    javascript

--- public/javascripts/cellscript.js
+++ public/javascripts/cellscript.js
@@ -1,3 +1,24 @@
 /**
  * @author Alice
  */
+
+function changeColor(arg, color){
+	$(arg).style.backgroundColor = color;
+	//$(arg).tween('opacity',0);
+}
+
+function shiftRight(arg){
+	var curPos = parseFloat($(arg).getStyle("left"));
+	var newPos = curPos + 20;
+	$(whichElement).tween('left', newPos);
+
+}
+
+// shifts menu element to the left when hovered //
+function shiftLeft(arg){
+	element = $(arg);
+	fx = new Fx.Styles(element, {duration:200, wait:false});
+	fx.start({
+		'left': -25,
+	});
+}
